ba219eafd41a434a2c4745fdea3f0daadf99edd9
Fabian Jäger <fabian@mailbutler.io>
2019-03-12 09:02:27 +0100

    debug output

--- lib/omniauth/strategies/meister_oauth2.rb
+++ lib/omniauth/strategies/meister_oauth2.rb
@@ -3,19 +3,16 @@ require 'omniauth/strategies/oauth2'
 module OmniAuth
   module Strategies
     class MeisterOauth2 < OmniAuth::Strategies::OAuth2
-
       option :name, 'meister_oauth2'
 
-      option :client_options, {
-               site: "https://www.mindmeister.com",
-               authorize_url: "https://www.mindmeister.com/oauth2/authorize",
-               token_url: "https://www.mindmeister.com/oauth2/token"
-             }
+      option :client_options,
+             site: 'https://www.mindmeister.com',
+             authorize_url: 'https://www.mindmeister.com/oauth2/authorize',
+             token_url: 'https://www.mindmeister.com/oauth2/token'
 
-      uid { raw_info["id"] }
+      uid { raw_info['id'] }
 
       info do
-        # raw_info.merge(token: access_token.token)
         {
           name: raw_info['name'],
           email: raw_info['email']
@@ -25,17 +22,15 @@ module OmniAuth
       extra do
         { raw_info: raw_info }
       end
-            
+
       def raw_info
+        log(:info, access_token.to_hash)
         @raw_info ||= access_token.get('https://www.mindmeister.com/api/v2/users/me').parsed
-      end
-
-      def request_phase
-        super
-      end
-
-      def callback_phase
-        super
+        log(:info, @raw_profile_info)
+        @raw_profile_info
+      rescue StandardError => e
+        log(:error, e)
+        raise
       end
     end
   end
